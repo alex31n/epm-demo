d2d789953941562a2793200fb8fb881445d8a334
Md Ashraful Islam <alex31n@gmail.com>
2024-03-24 23:07:23 +0600

    initialize spring project

---
+++ src/main/java/com/bits/epm/EpmApplication.java
@@ -0,0 +1,13 @@
+package com.bits.epm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpmApplication.class, args);
+	}
+
+}
