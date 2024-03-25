4a916f93d22a6de55e9258f51b400f823bef0a24
Md Ashraful Islam <alex31n@gmail.com>
2024-03-26 02:48:21 +0600

    feat: add web_security_config

--- src/main/java/com/bits/epm/config/WebSecurityConfig.java
+++ src/main/java/com/bits/epm/config/WebSecurityConfig.java
@@ -1,7 +1,59 @@
 package com.bits.epm.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authz -> authz
+                .requestMatchers("/auth/**", "/js/**", "/css/**","/img/**").permitAll()
+//                .requestMatchers("/h2/**").permitAll()
+                .requestMatchers("/").permitAll()
+//                .requestMatchers("/logout").permitAll()
+                .anyRequest().authenticated()
+        );
+
+        http.logout(logout->
+                logout.invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User
+                .withUsername("user")
+                .password(passwordEncoder.encode("123456"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(Arrays.asList(user,admin));
+    }
 }
